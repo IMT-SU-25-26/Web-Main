b969fb0ce94c8fbe95d48692421c408810c30667
felix <ffelixrichardo@gmail.com>
2025-07-27 08:49:18 +0700

    rebase activities to achievements so get the latest update

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement } from 'react'
+
+type ButtonProps = {
+    bgColor:string,
+    children:React.ReactNode,
+    className?:string,
+}
+
+const Button = ({bgColor, children, className}:ButtonProps) => {
+  return (
+    <button
+        type="button"
+        className={`transition-all duration-300 hover:shadow-[0_0_10px_4px] hover:ring-2 text-white text-[12px] px-4 py-1 rounded-md ease-in-out hover:brightness-90 ${className}`}
+        style={{ backgroundColor: bgColor }}
+        >
+        {children}
+    </button>
+  )
+}
+
+export default Button
