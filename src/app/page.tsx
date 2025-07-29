3b77f48782bedb774c7464b921fba43901de3b79
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-07-29 19:28:20 +0700

    background assets added, responsiveness dekstop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,15 @@ export default function Home() {
             draggable="false"
             width={628}
             height={380}
-            alt="mid-bubble"
+            alt="dragon"
+          ></Image>
+          <Image
+            className="yellow-star-landing absolute z-[9] w-[10rem] h-auto top-[30%] -left-[15%]"
+            src={"/home/yellow-star-landing.webp"}
+            draggable="false"
+            width={628}
+            height={380}
+            alt="yellow-star-landing"
           ></Image>
           <Image
             className="among-us absolute z-[9] w-[8rem] h-auto bottom-[4rem] -right-[12rem]"
@@ -116,11 +124,21 @@ export default function Home() {
             height={1080}
             alt="sobekan"
           ></Image>
-          <h1 className="what-is-su-imt-text mt-[-10%] relative z-10 text-6xl font-bold text-white font-family-impact">WHAT IS SU IMT</h1>
+          <h1 className="what-is-su-imt-text mt-[-10%] relative z-10 text-6xl font-bold text-white font-family-impact">
+            WHAT IS SU IMT
+          </h1>
           <p className="su-imt-description relative z-10 text-xl text-white font-bold max-w-2xl text-center">
-            Student Union Information and Multimedia Technology is a student-led organization that serves as a platform for students of the Informatics study program to grow professionally, socially, and academically. SU IMT UC organizes various events, workshops, and initiatives to enhance student engagement, develop leadership skills, and support innovation in technology.
+            Student Union Information and Multimedia Technology is a student-led
+            organization that serves as a platform for students of the
+            Informatics study program to grow professionally, socially, and
+            academically. SU IMT UC organizes various events, workshops, and
+            initiatives to enhance student engagement, develop leadership
+            skills, and support innovation in technology.
           </p>
-          <Link href="/" className="learn-more-button relative font-family-impact z-10 text-4xl bg-[#0E54B2] p-6 px-10 rounded-2xl text-white font-bold">
+          <Link
+            href="/"
+            className="learn-more-button relative font-family-impact z-10 text-4xl bg-[#0E54B2] p-6 px-10 rounded-2xl text-white font-bold"
+          >
             Learn More
           </Link>
           <Image
@@ -193,10 +211,18 @@ export default function Home() {
 
         {/* Our values section */}
         <div className="competition-wrapper relative w-screen flex flex-col justify-start items-center pb-[15%]">
-          <h1 className="competition-text font-family-gill-condensed-bold font-extrabold text-6xl">COMPETITIONS</h1>
+          <h1 className="competition-text font-family-gill-condensed-bold font-extrabold text-6xl">
+            COMPETITIONS
+          </h1>
           <div className="competition-right-left-container mt-14 w-full h-full flex gap-[10rem] justify-center items-center">
             <div className="w-full h-full relative z-10 competition-left-container">
-              <Image className="w-[85%] h-auto" src={"/home/compete-bg.svg"} width={500} height={500} alt="background-compete"></Image>
+              <Image
+                className="w-[85%] h-auto"
+                src={"/home/compete-bg.svg"}
+                width={500}
+                height={500}
+                alt="background-compete"
+              ></Image>
               <Image
                 className="absolute z-10 w-[100%] h-auto top-0 left-5"
                 src={"/home/compete.svg"}
@@ -205,43 +231,157 @@ export default function Home() {
                 height={500}
                 alt="competition-left"
               ></Image>
+               <Image
+                className="light-bulp absolute z-10 w-[35%] h-auto top-[-60%] left-0"
+                src={"/home/light-bulp.webp"}
+                draggable="false"
+                width={500}
+                height={500}
+                alt="competition-left"
+              ></Image>
               <div className="join-now-text-container absolute w-[350px] h-[120px] bottom-[-5%] left-[47.5%] bg-[#0E54B2] p-4 px-8 rounded-2xl z-[2]">
                 <div className="bg-[#ED427C] flex items-center justify-center w-full h-full absolute left-[5%] top-[-10%] rounded-2xl z-[-1]">
-                  <h1 className="join-now-text font-family-impact text-6xl text-center text-white">Join Now</h1>
+                  <h1 className="join-now-text font-family-impact text-6xl text-center text-white">
+                    Join Now
+                  </h1>
                 </div>
               </div>
             </div>
-            <div className="competition-right-container w-full h-full">
-              <div className="competition-right-background relative rotate-[-2.5deg] w-[40rem] h-[27.5rem] bg-white flex flex-col gap-4 p-4">
-                <Image className="competition-right-handle absolute top-[-25%] right-[40%] w-[7.5rem] rotate-90" src={"/home/handle-card.png"} width={500} height={500} alt="handle-card"></Image>
-                <Image className="competition-star absolute top-[-15%] right-[-8%] w-[10rem]" src={"/home/star-card.png"} width={500} height={500} alt="star-card"></Image>
+            <div className="relative competition-right-container w-full h-full">
+              <div className="competition-right-background relative z-[2] rotate-[-2.5deg] w-[40rem] h-[27.5rem] bg-white flex flex-col gap-4 p-4">
+                <Image
+                  className="competition-right-handle absolute top-[-25%] right-[40%] w-[7.5rem] rotate-90"
+                  src={"/home/handle-card.png"}
+                  width={500}
+                  height={500}
+                  alt="handle-card"
+                ></Image>
+                <Image
+                  className="competition-star absolute top-[-15%] right-[-8%] w-[10rem]"
+                  src={"/home/star-card.png"}
+                  width={500}
+                  height={500}
+                  alt="star-card"
+                ></Image>
+                <Image
+                  className="round-spike absolute bottom-[-30%] right-[75%] w-[40%]"
+                  src={"/home/round-spike.webp"}
+                  width={500}
+                  height={500}
+                  alt="round-spike"
+                ></Image>
                 <div className="image-container bg-gray-300 w-full h-1/2"></div>
-                <h1 className="competition-title-text font-family-impact text-4xl">HACKATHON 2025</h1>
-                <p className="competition-description-text text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, omnis?</p>
-                <p className="competition-member-text text-xl text-end">Team: 3-4 members</p>
+                <h1 className="competition-title-text font-family-impact text-4xl">
+                  HACKATHON 2025
+                </h1>
+                <p className="competition-description-text text-xl">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Dolores, omnis?
+                </p>
+                <p className="competition-member-text text-xl text-end">
+                  Team: 3-4 members
+                </p>
+              </div>
+               <Image
+                  className="red-spinner absolute bottom-[-30%] right-[-10%] w-[35%]"
+                  src={"/home/red-spinner.svg"}
+                  width={500}
+                  height={500}
+                  alt="round-spike"
+                ></Image>
+            </div>
+          </div>
+          <Image src={"/home/green-line.svg"} alt="green-line" width={900} height={900} className="green-line absolute right-[-10%] top-[-45%] w-[40%]"></Image>
+        </div>
+
+        <div className="relative z-[3] w-screen pb-[10%] flex flex-col justify-start items-center">
+          <h1 className="explore-text font-family-gill-condensed-bold font-extrabold text-6xl">
+            EXPLORE OUR
+          </h1>
+          <h1 className="explore-text font-family-gill-condensed-bold font-extrabold text-6xl">
+            COMMUNITY SERVICES
+          </h1>
+          <div className="home-service-card-container relative flex mt-14">
+            <HomeServicesCard
+              className="home-service-card-left absolute left-[25%] top-[3rem] max-h-[18rem]"
+              title="Title"
+              type="Achievements"
+              description="Description for Service 1"
+              color="#0555AB"
+              opacity="75%"
+            />
+            <HomeServicesCard
+              className="home-service-card-center relative z-10"
+              title="Title"
+              type="Achievements"
+              description="Description for Service 1"
+              color="#0555AB"
+              opacity="100%"
+            />
+            <HomeServicesCard
+              className="home-service-card-right absolute right-[20%] top-[3rem] max-h-[18rem]"
+              title="Title"
+              type="Achievements"
+              description="Description for Service 1"
+              color="#0555AB"
+              opacity="75%"
+            />
+          </div>
+          <div className="relative flex justify-center items-center w-full h-full mt-[10rem]">
+            <div className="explore-button absolute w-[350px] h-[120px] bg-[#0E54B2] p-4 px-8 rounded-2xl z-[2]">
+              <div className="bg-[#ED427C] flex items-center justify-center w-full h-full absolute left-[5%] top-[-10%] rounded-2xl z-[-1]">
+                <h1 className="explore-button-text font-family-impact text-6xl text-center text-white">
+                  EXPLORE
+                </h1>
               </div>
             </div>
           </div>
+           <Image
+            className="paper-cloud w-[20%] top-[-5rem] h-auto absolute left-0"
+            src={"/home/paper-cloud.webp"}
+            draggable="false"
+            width={1920}
+            height={1080}
+            alt="footer"
+          ></Image>
+          <Image
+            className="w-[15%] top-[0rem] h-auto absolute right-0"
+            src={"/home/green-arrow-home.png"}
+            draggable="false"
+            width={1920}
+            height={1080}
+            alt="footer"
+          ></Image>
+          <Image
+            className="red-star-footer-left w-[20%] top-[35%] h-auto absolute left-0"
+            src={"/home/orange-star-footer-left.png"}
+            draggable="false"
+            width={1920}
+            height={1080}
+            alt="footer"
+          ></Image>
         </div>
 
-        <div className="relative w-screen pb-[10%] flex flex-col justify-start items-center overflow-hidden">
-           <h1 className="explore-text font-family-gill-condensed-bold font-extrabold text-6xl">EXPLORE OUR</h1>
-           <h1 className="explore-text font-family-gill-condensed-bold font-extrabold text-6xl">COMMUNITY SERVICES</h1>
-           <div className="home-service-card-container relative flex mt-14">
-            <HomeServicesCard className="home-service-card-left absolute left-[25%] top-[3rem] max-h-[18rem]" title="Title" type="Achievements" description="Description for Service 1" color="#0555AB" opacity="75%"/>
-            <HomeServicesCard className="home-service-card-center relative z-10" title="Title" type="Achievements" description="Description for Service 1" color="#0555AB" opacity="100%" />
-            <HomeServicesCard className="home-service-card-right absolute right-[20%] top-[3rem] max-h-[18rem]" title="Title" type="Achievements" description="Description for Service 1" color="#0555AB" opacity="75%"/>
-           </div>
-           <div className="relative flex justify-center items-center w-full h-full mt-[10rem]">
-           <div className="explore-button absolute w-[350px] h-[120px] bg-[#0E54B2] p-4 px-8 rounded-2xl z-[2]">
-                <div className="bg-[#ED427C] flex items-center justify-center w-full h-full absolute left-[5%] top-[-10%] rounded-2xl z-[-1]">
-                  <h1 className="explore-button-text font-family-impact text-6xl text-center text-white">EXPLORE</h1>
-                </div>
-              </div>
-            </div>
+        <div className="relative z-[2] w-full">
+          <Image
+            className="w-[30%] top-[-15rem] h-auto absolute left-0"
+            src={"/home/green-home-footer-left.svg"}
+            draggable="false"
+            width={1920}
+            height={1080}
+            alt="footer"
+          ></Image>
+          <Image
+            className="yellow-spike-right w-[15%] top-[-20rem] h-auto absolute right-0"
+            src={"/home/yellow-spike-right.png"}
+            draggable="false"
+            width={1920}
+            height={1080}
+            alt="footer"
+          ></Image>
         </div>
 
-        <div className="h-[10vh] w-screen bg-[#E44843]"></div>
+        <div className="relative z-10 h-[10vh] w-screen bg-[#E44843]"></div>
       </div>
     </>
   );
