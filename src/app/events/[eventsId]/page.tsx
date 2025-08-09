7eb49dff04ddf1a6c8955aa2988786021ae842cb
Nicho <nicholasleroy83@gmail.com>
2025-08-09 18:15:23 +0700

    fix pnpm build

--- src/app/events/[eventsId]/page.tsx
+++ src/app/events/[eventsId]/page.tsx
@@ -1,13 +1,15 @@
-export default function EventPage({
+export default async function EventPage({
   params,
 }: {
-  params: { eventsId: string };
+  params: Promise<{ eventsId: string }>;
 }) {
+  const { eventsId } = await params;
+
   return (
     <div className="w-full overflow-hidden">
       <div className="h-[10vh] bg-[#F1EEE6]"></div>
       <div className="flex flex-col gap-15 items-center min-h-screen w-full bg-[url('/backgrounds/background-paper.png')] bg-contain bg-center bg-[#F1EEE6] overflow-hidden">
-        <div>Event Page: {params.eventsId}</div>
+        <div>Event Page: {eventsId}</div>
       </div>
     </div>
   );
